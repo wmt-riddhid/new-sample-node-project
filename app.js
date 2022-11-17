1e10ee3736580613bd0ac6efdddc2c3129070b53
Administrator <riddhid@webmobtech.com>
2022-11-17 12:21:01 +0530

    to deploy on netlify

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
-/*
-
-GEREKLİ PAKETLER YÜKLENİYOR...
-
-*/
-var http = require('http');
-var express = require('express');
-
-var app = express();
-
-app.set('port', process.env.PORT || 3005); // GİRİŞ PORTU AYARLANDI
-app.set('views', __dirname + '/app/server/views'); // VIEW KLASÖRÜ TANITILDI
-app.set('view engine', 'ejs'); // VIEW ENGINE AYARLANDI
-app.use(express.static(__dirname + '/app/public')); // KULLANICILAR TARAFINDAN ERİŞİLEBİLEN KLASÖR TANIMLANDI
-
-require('./app/routes')(app); // ROUTE DOSYASI ÇAĞIRILDI
-
-/*
-
-HTTP SERVER OLUŞTURULDU
-
-*/
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Sistem ' + app.get('port') + ' Portu Üzerinde Çalışıyor.');
+const express = require("express");
+const app = express();
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+app.listen(3000, function () {
+    console.log("Server is running on localhost3000");
 });
